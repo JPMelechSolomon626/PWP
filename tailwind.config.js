ee98b61a27dd3425e5463cfcd6f4ca5e0a12c71d
JP Melech Solomon <jmelechsolomon@cnm.edu>
2023-10-16 16:58:16 -0600

    end of the day commit and push

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,32 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js}"],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+
+          "primary": "#556b2f",
+
+          "secondary": "#baa693",
+
+          "accent": "#a0403e",
+
+          "neutral": "#2a323c",
+
+          "base-100": "#1d232a",
+
+          "info": "#1a1952",
+
+          "success": "#4169e1",
+
+          "warning": "#fbbd23",
+
+          "error": "#f87272",
+        },
+      },
+    ],
+  },
   theme: {
     extend: {},
   },
